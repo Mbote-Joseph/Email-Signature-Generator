4790449b6f662c30a4c71a60a3ccd96d9e8e3907
Joseph Mbote <mbotejoseph001@gmail.com>
2020-07-20 19:30:42 +0300

    third commit

--- src/app/components/general-form/general-form.component.ts
+++ src/app/components/general-form/general-form.component.ts
@@ -8,11 +8,39 @@ import { NgForm } from '@angular/forms';
 })
 export class GeneralFormComponent implements OnInit {
 @ViewChild ('form') generalForm:NgForm;
+
+  user={
+    name:'',
+    company:'',
+    position:'',
+    department:'',
+    phone:'',
+    mobile:'',
+    website:'',
+    skype:'',
+    email:'',
+    address:''
+  };
+  submitted=false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
  onSubmit(){
+   this.submitted=true;
+   this.user.name=this.generalForm.value.name;
+   this.user.company=this.generalForm.value.company;
+   this.user.position=this.generalForm.value.position;
+   this.user.department=this.generalForm.value.department;
+   this.user.phone=this.generalForm.value.phone;
+   this.user.mobile=this.generalForm.value.mobile;
+   this.user.website=this.generalForm.value.website;
+   this.user.skype=this.generalForm.value.skype;
+   this.user.email=this.generalForm.value.email;
+   this.user.address=this.generalForm.value.address;
    console.log(this.generalForm);
+   this.generalForm.reset();
+   
  }
 }
